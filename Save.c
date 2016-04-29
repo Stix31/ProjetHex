51a7b17529c97ce6f47ee400c71f05e8b1f06f28
karimx10 <karimsalama94@hotmail.com>
2016-04-29 15:55:13 +0200

    enfin..

--- Save.c
+++ Save.c
@@ -5,26 +5,25 @@
 #include "Save.h"
 
 void saveString(int x,int y, int player, char* turn){
-    sprintf(turn, "%d,%d,%d\n",player,x,y);
+	sprintf(turn, "%d,%d,%d\n",player,x,y);
 }
 
 void saveTurn (int x,int y, int player){
-    FILE* fHistorique = NULL;
-    fHistorique = fopen("historique.sav", "a");
-    if (fHistorique != NULL){
-      char turn[16];
-      saveString(x,y,player,turn);
-      printf("turn %s\n",turn);
-      fputs(turn, fHistorique);
-    FILE* fHistorique = fopen("historique.sav", "a");
-    if (!fHistorique){
-        /* mode == 1 -> sauvegarde / mode != 1 -> charge */
-        if (mode == 1){
-            char turn[16];
-            saveString(x,y,player,turn);
-            printf("turn %s\n",turn);
-            fputs(turn, fHistorique);
-        }
-    }
-    fclose(fHistorique);
+	FILE* fHistorique = NULL;
+	fHistorique = fopen("historique.sav", "a");
+	if (fHistorique != NULL){
+		char turn[16];
+		saveString(x,y,player,turn);
+		printf("turn %s\n",turn);
+		fputs(turn, fHistorique);
+		FILE* fHistorique = fopen("historique.sav", "a");
+		if (!fHistorique){
+			/* mode == 1 -> sauvegarde / mode != 1 -> charge */
+			char turn[16];
+			saveString(x,y,player,turn);
+			printf("turn %s\n",turn);
+			fputs(turn, fHistorique);
+		}
+		fclose(fHistorique);
+	}
 }
